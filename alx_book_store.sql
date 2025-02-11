abf010a94466be66aa2a3c6e401c7ffbc566ea42
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-11 23:55:05 +0100

    Created a database for an online bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,5 @@
 /*Learning to wrok with databases by Creating a database for an online book store*/
-
+CREATE DATABASE IF NOT EXISTS alx_book_store
 CREATE TABLE Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
